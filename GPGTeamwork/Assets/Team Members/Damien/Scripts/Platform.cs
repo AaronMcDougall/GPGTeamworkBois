e0f7172563979094aa08a69aeafc1e97c1ef1b59
Damien Harwood <1019243@student.sae.edu.au>
2021-06-16 17:06:20 +1000

    More Prefabs and GameManager

---
+++ GPGTeamwork/Assets/Team Members/Damien/Scripts/Platform.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public GameObject thisPlatform;
+    void Start()
+    {
+        thisPlatform = gameObject;
+    }
+
+    // Update is called once per frame
+    
+}
