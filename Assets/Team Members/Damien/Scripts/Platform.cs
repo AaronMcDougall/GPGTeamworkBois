d0743cbacb350f24721f28243faad0dbeda4e356
Damien <1019243@student.sae.edu.au>
2021-06-29 16:32:30 +1000

    Platform timers and networking

--- Assets/Team Members/Damien/Scripts/Platform.cs
+++ Assets/Team Members/Damien/Scripts/Platform.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using Damien;
+using Mirror;
 using UnityEngine;
 
-public class Platform : MonoBehaviour
+public class Platform : NetworkBehaviour
 {
     public GameObject thisPlatform;
-    void Start()
+    public GameManager gameManager;
+
+    void Awake()
     {
-        thisPlatform = gameObject;
+        if (isServer)
+        {
+            thisPlatform = gameObject;
+            gameManager.TimerStart += RPCCountdown;
+        }
     }
 
-    
-    
+    [ClientRpc]
+    void RPCCountdown()
+    {
+        
+    }
 }
